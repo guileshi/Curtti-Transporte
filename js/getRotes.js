93626ec997080b1499f50f526925568786f6d2c2
guileshi <btguile@hotmail.com>
2022-04-12 16:54:19 -0300

    Mudanças na pagina inicio e de cotação

---
+++ js/getRotes.js
@@ -0,0 +1,24 @@
+function getRotes(el) {
+
+    localStorage.clear()
+
+    var origin = el.querySelector(".origin").innerHTML
+    var destination = el.querySelector(".destination").innerHTML
+
+    localStorage.setItem('Origem', origin)
+    localStorage.setItem('Destino', destination)
+}
+
+function setRotes() {
+
+    if (localStorage.length >= 1) {
+        var originInput = document.querySelector('#origin')
+        var destinationInput = document.querySelector('#destination')
+
+        originInput.value = localStorage.getItem('Origem')
+        destinationInput.value = localStorage.getItem('Destino')
+
+        localStorage.clear()
+    }
+
+}
